0645d092ce71ea71b9b98488fc51eaee1148972e
ahlier <66384646+ahlier@users.noreply.github.com>
2023-04-13 14:06:22 -0400

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -1,9 +1,10 @@
-import math
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 from scipy import special
-import time
+'''
+This program can draw binomial, poisson and normal distribution
+'''
 
 
 def C(n, r):
@@ -18,6 +19,10 @@ def normal(x, sigma, miu):
     return np.e**(-0.5*((x-miu)/sigma)**2)/(sigma*np.sqrt(2*np.pi))
 
 
+def poisson(x, EV):
+    return (EV**x)*(np.e**-EV)/(special.factorial(x))
+
+
 def draw_binomial(n = 100, p = 1/6):
     p_lst = []
     for i in range(n):
@@ -25,8 +30,18 @@ def draw_binomial(n = 100, p = 1/6):
     popt, pcov = curve_fit(normal, np.arange(n), p_lst)
     plt.plot(np.arange(n*10)/10, normal(np.arange(n*10)/10, *popt))
     plt.hist(np.arange(n), weights=p_lst, bins=n, rwidth=0.9)
+    plt.title('binomial distribution')
     plt.show()
 
 
+def draw_poisson(x, EV):
+    plt.plot(x, poisson(x, EV))
+    plt.title('poisson distribution')
+    plt.show()
 
+
+def draw_normal(sigma, miu, start=0, end=100):
+    plt.plot(np.arange(start, end, 0.1), normal(np.arange(start, end, 0.1), sigma, miu))
+    plt.title('normal distribution')
+    plt.show()
 
